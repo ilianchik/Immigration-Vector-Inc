f9309daf44896152823dd85be8d7f7a960d23758
ilianchik <kozachishin543@gmail.com>
2023-06-07 17:06:16 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 window.addEventListener("scroll", function () {
   var logoContainer = document.querySelector(".logo");
+  var logoImg = document.querySelector(".logo__img");
   if (window.scrollY > 0) {
     logoContainer.classList.add("scrolled");
+    logoImg.src = "../sourse/img/logo2.png";
   } else {
     logoContainer.classList.remove("scrolled");
+    logoImg.src = "../sourse/img/logo.png";
   }
 });
 
@@ -28,6 +31,14 @@ window.addEventListener("scroll", function () {
 window.addEventListener("scroll", function () {
   var textContainer = document.querySelector(".menu");
   if (window.scrollY > 450) {
+    textContainer.classList.add("scrolled");
+  } else {
+    textContainer.classList.remove("scrolled");
+  }
+});
+window.addEventListener("scroll", function () {
+  var textContainer = document.querySelector(".main__left-box2");
+  if (window.scrollY > 750) {
     textContainer.classList.add("scrolled");
   } else {
     textContainer.classList.remove("scrolled");
