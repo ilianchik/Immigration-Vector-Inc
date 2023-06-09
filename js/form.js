fb47e68bb2cf04cf8fd816dff5558d9975817e16
ToriToriToriToriTori <toporovskaviktoria@gmail.com>
2023-06-09 15:50:47 +0300

    Get in touch animation backgroung scroll down

--- js/form.js
+++ js/form.js
@@ -1,10 +1,19 @@
 function toggleGetInTouch() {
     const getInTouch = document.querySelector('.get_in_touch');
+    var elements = document.querySelectorAll('.get_in_touch__reaction');
+
     if (getInTouch.classList.contains('opened')) {
       getInTouch.classList.remove('opened');
       getInTouch.classList.add('closed');
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('get_in_touch__opened');
+      }
     } else {
+
       getInTouch.classList.remove('closed');
       getInTouch.classList.add('opened');
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('get_in_touch__opened');
+      }
     }
   }
