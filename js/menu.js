13dae15d5ba038a1bcb8181261dac4c9eeb1ba05
ToriToriToriToriTori <toporovskaviktoria@gmail.com>
2023-06-12 20:04:53 +0300

    non scroll menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+var SCROLL = 0; 
+
 function toggleMenu() {
   const menuBtn = document.querySelector(".menu__btn");
 
@@ -6,20 +8,34 @@ function toggleMenu() {
   var logoContainer = document.querySelector(".logo");
   var logoImg = document.querySelector(".logo__img");
 
-  if (menuBtn.classList.contains("closed")) {
-    setTimeout(function () {
-      menuBtn.classList.remove("closed");
-      menuBtn.classList.add("opened");
-      menuBody.style.opacity = "1";
+  if (menuBtn.classList.contains("closed")) {      
+      SCROLL = window.scrollY;
+      console.log(SCROLL);
 
-      menuBody.classList.remove("closed");
-      menuBody.classList.add("opened");
-    }, 500);
+      if(!logoContainer.classList.contains('.scrolled')){
+        setTimeout(function () {
+          menuBtn.classList.remove("closed");
+          menuBtn.classList.add("opened");
+          menuBody.style.opacity = "1";
+          
+          menuBody.classList.remove("closed");
+          menuBody.classList.add("opened");
+        }, 500);
+      } else{
+        menuBtn.classList.remove("closed");
+        menuBtn.classList.add("opened");
+        menuBody.style.opacity = "1";
+        
+        menuBody.classList.remove("closed");
+        menuBody.classList.add("opened");
+      }
 
+    
     logoContainer.classList.add("moved");
     logoImg.src = "./sourse/img/logo2.png";
 
-    document.body.classList.add("noscroll");
+    setTimeout(function () {
+    document.body.classList.add("noscroll");}, 2000);
   } else if (menuBtn.classList.contains("opened")) {
     menuBtn.classList.remove("opened");
     menuBtn.classList.add("closed");
@@ -28,7 +44,7 @@ function toggleMenu() {
     }, 2000);
     menuBody.classList.remove("opened");
     menuBody.classList.add("closed");
-
+    
     setTimeout(function () {
       logoContainer.classList.remove("moved");
       if (!logoContainer.classList.contains("scrolled")) {
@@ -37,5 +53,6 @@ function toggleMenu() {
     }, 500);
 
     document.body.classList.remove("noscroll");
+    window.scrollTo(0, SCROLL);
   }
 }
