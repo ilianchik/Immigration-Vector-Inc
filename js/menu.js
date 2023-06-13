fd7181895d1711e5490b8efffd7b9885d9463e2f
Ilia Kozachyshyn <kozachishin543@gmail.com>
2023-06-13 17:06:43 +0300

    new

--- js/menu.js
+++ js/menu.js
@@ -10,83 +10,62 @@ function toggleMenu() {
   var logoImg = document.querySelector(".logo__img");
 
   if (menuBtn.classList.contains("closed")) {
-        //set scroll var
-        SCROLL = window.scrollY;
-        console.log(SCROLL);
+    // open menu
+    //set scroll var
+    SCROLL = window.scrollY;
+    console.log(SCROLL);
 
-        //set time if logo isn`t moved
-        if (!logoContainer.classList.contains(".scrolled")) {
+    //set time if logo isn`t moved
+    if (!logoContainer.classList.contains(".scrolled")) {
+      setTimeout(function () {
+        menuBtn.classList.remove("closed");
+        menuBtn.classList.add("opened");
+        menuBody.style.opacity = "1";
 
-          setTimeout(function () {
-            menuBtn.classList.remove("closed");
-            menuBtn.classList.add("opened");
-            menuBody.style.opacity = "1";
+        menuBody.classList.remove("closed");
+        menuBody.classList.add("opened");
+      }, 500);
+    } else {
+      //same code without timeset
+      menuBtn.classList.remove("closed");
+      menuBtn.classList.add("opened");
+      menuBody.style.opacity = "1";
 
-            menuBody.classList.remove("closed");
-            menuBody.classList.add("opened");
-          }, 500);
+      menuBody.classList.remove("closed");
+      menuBody.classList.add("opened");
+    }
 
-        } else {
-          //same code without timeset
-          menuBtn.classList.remove("closed");
-          menuBtn.classList.add("opened");
-          menuBody.style.opacity = "1";
+    //change logo
+    logoContainer.classList.add("moved");
+    logoImg.src = "./sourse/img/logo2.png";
 
     setTimeout(function () {
       document.body.classList.add("noscroll");
     }, 2000);
-    logoImg.src = "./sourse/img/logo2.png";
-  } 
-    else if (menuBtn.classList.contains("opened")) {
+  } else if (menuBtn.classList.contains("opened")) {
+    //close menu
+
+    // close menu
     menuBtn.classList.remove("opened");
     menuBtn.classList.add("closed");
+
     setTimeout(function () {
       menuBody.style.opacity = "0";
     }, 2000);
+
     menuBody.classList.remove("opened");
     menuBody.classList.add("closed");
-          menuBody.classList.remove("closed");
-          menuBody.classList.add("opened");
-        }
 
-        //change logo
-        logoContainer.classList.add("moved");
-        if(lo)
-        logoImg.src = "./sourse/img/logo2.png";
-
-        setTimeout(function () {
-          document.body.classList.add("noscroll");
-        }, 2000);
-        
-  } else if (menuBtn.classList.contains("opened")) { //close menu
-
-    // close menu
-        menuBtn.classList.remove("opened");
-        menuBtn.classList.add("closed");
-
-        setTimeout(function () {
-          menuBody.style.opacity = "0";
-        }, 2000);
-
-        menuBody.classList.remove("opened");
-        menuBody.classList.add("closed");
-
-        //logo change
-        setTimeout(function () {
-          logoContainer.classList.remove("moved");
-          
-          if (!logoContainer.classList.contains("scrolled")) {
-            logoImg.src = "./sourse/img/logo.png";
-          }
-        }, 500);
-
-        //scroll to
-        document.body.classList.remove("noscroll");
-        window.scrollTo(0, SCROLL);
+    //logo change
+    setTimeout(function () {
+      logoContainer.classList.remove("moved");
+      if (!logoContainer.classList.contains("scrolled")) {
+        logoImg.src = "./sourse/img/logo.png";
       }
+    }, 500);
+
+    //scroll to
+    document.body.classList.remove("noscroll");
+    window.scrollTo(0, SCROLL);
+  }
 }
-const button = document.getElementById("toggleFixed");
-button.addEventListener("click", () => {
-  document.body.classList.toggle("noscroll");
-  button.style.position = "fixed";
-});
