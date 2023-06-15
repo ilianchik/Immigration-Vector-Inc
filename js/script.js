f87ef4242c8e3c1579305dd8a9f7258c5a582ea2
Ilia Kozachyshyn <kozachishin543@gmail.com>
2023-06-15 13:38:14 +0300

    prefixes

--- js/script.js
+++ js/script.js
@@ -59,69 +59,75 @@ window.addEventListener("scroll", function () {
     textContainer.classList.remove("scrolled");
   }
 });
+if (window.innerWidth >= 848) {
+  window.addEventListener("scroll", function () {
+    var textContainer = document.querySelector(".menu");
+    var windowHeight = window.innerHeight;
+    var percentageOffset = windowHeight * 0.55;
 
-window.addEventListener("scroll", function () {
-  var textContainer = document.querySelector(".menu");
-  var windowHeight = window.innerHeight;
-  var percentageOffset = windowHeight * 0.55;
+    const menuBtn = document.querySelector(".menu__btn");
 
-  const menuBtn = document.querySelector(".menu__btn");
-
-  if (!menuBtn.classList.contains("opened")) {
-    if (window.scrollY > percentageOffset) {
-      textContainer.classList.add("scrolled");
-    } else {
-      textContainer.classList.remove("scrolled");
+    if (!menuBtn.classList.contains("opened")) {
+      if (window.scrollY > percentageOffset) {
+        textContainer.classList.add("scrolled");
+      } else {
+        textContainer.classList.remove("scrolled");
+      }
     }
-  }
-});
-
-window.addEventListener("scroll", function () {
-  var textContainer = document.querySelector(".testimonial");
-  var windowHeight = window.innerHeight;
-  var percentageOffset = windowHeight * 1;
-  if (window.scrollY > percentageOffset) {
-    textContainer.classList.add("scrolled");
-  } else {
-    textContainer.classList.remove("scrolled");
-  }
-});
-
-window.addEventListener("scroll", function () {
-  var textContainer = document.querySelector(".services");
-  var windowHeight = window.innerHeight;
-  var percentageOffset = windowHeight * 1.9;
-  if (window.scrollY > percentageOffset) {
-    textContainer.classList.add("scrolled");
-  } else {
-    textContainer.classList.remove("scrolled");
-  }
-});
-window.addEventListener("scroll", function () {
-  var textContainers = document.querySelectorAll(".advantages__content-box");
-  var windowHeight = window.innerHeight;
-  var percentageOffset = windowHeight * 2.2;
-  textContainers.forEach(function (textContainer) {
+  });
+}
+if (window.innerWidth >= 848) {
+  window.addEventListener("scroll", function () {
+    var textContainer = document.querySelector(".testimonial");
+    var windowHeight = window.innerHeight;
+    var percentageOffset = windowHeight * 1;
     if (window.scrollY > percentageOffset) {
       textContainer.classList.add("scrolled");
     } else {
       textContainer.classList.remove("scrolled");
     }
   });
-});
-
-window.addEventListener("scroll", function () {
-  var icons = document.querySelectorAll(".advantages__img");
-  var windowHeight = window.innerHeight;
-  var percentageOffset = windowHeight * 2.2;
-  icons.forEach(function (icon) {
+}
+if (window.innerWidth >= 848) {
+  window.addEventListener("scroll", function () {
+    var textContainer = document.querySelector(".services");
+    var windowHeight = window.innerHeight;
+    var percentageOffset = windowHeight * 1.9;
     if (window.scrollY > percentageOffset) {
-      icon.classList.add("advantages__img--open");
+      textContainer.classList.add("scrolled");
     } else {
-      icon.classList.remove("advantages__img--open");
+      textContainer.classList.remove("scrolled");
     }
   });
-});
+}
+if (window.innerWidth >= 848) {
+  window.addEventListener("scroll", function () {
+    var textContainers = document.querySelectorAll(".advantages__content-box");
+    var windowHeight = window.innerHeight;
+    var percentageOffset = windowHeight * 2.2;
+    textContainers.forEach(function (textContainer) {
+      if (window.scrollY > percentageOffset) {
+        textContainer.classList.add("scrolled");
+      } else {
+        textContainer.classList.remove("scrolled");
+      }
+    });
+  });
+}
+if (window.innerWidth >= 848) {
+  window.addEventListener("scroll", function () {
+    var icons = document.querySelectorAll(".advantages__img");
+    var windowHeight = window.innerHeight;
+    var percentageOffset = windowHeight * 2.2;
+    icons.forEach(function (icon) {
+      if (window.scrollY > percentageOffset) {
+        icon.classList.add("advantages__img--open");
+      } else {
+        icon.classList.remove("advantages__img--open");
+      }
+    });
+  });
+}
 
 // $(".slider").slick({
 //   arrows: false,
