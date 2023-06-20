07e4e4a699b56463f6de095b38ce2fff9594f07a
Ilia Kozachyshyn <kozachishin543@gmail.com>
2023-06-20 19:55:51 +0300

    cringe

--- js/form.js
+++ js/form.js
@@ -20,60 +20,58 @@ fetch("https://restcountries.com/v3.1/all?fields=name")
   });
 
 function toggleGetInTouch() {
-  setTimeout(function () {
-    const getInTouch = document.querySelector(".get_in_touch");
-    var elements = document.querySelectorAll(".get_in_touch__reaction");
-    const btn = document.querySelector(".get_in_touch__btn");
+  const getInTouch = document.querySelector(".get_in_touch");
+  var elements = document.querySelectorAll(".get_in_touch__reaction");
+  const btn = document.querySelector(".get_in_touch__btn");
 
-    const form = document.querySelector("get_in_touch__form");
+  const form = document.querySelector("get_in_touch__form");
 
-    var heroTextMobile = document.querySelector(".hero__text--mobile");
-    var heroArrowMobile = document.querySelector(".hero__arrow-btn--mobile");
-    const leftSide = document.querySelector(".grid__left");
+  var heroTextMobile = document.querySelector(".hero__text--mobile");
+  var heroArrowMobile = document.querySelector(".hero__arrow-btn--mobile");
+  const leftSide = document.querySelector(".grid__left");
 
-    if (getInTouch.classList.contains("opened")) {
-      //close
-      btn.textContent = "Get in touch";
-      if (window.innerWidth <= 848) {
-        heroTextMobile.classList.remove("scrolled");
-        heroArrowMobile.classList.remove("scrolled");
-        leftSide.style.transform = "translateX(0%)";
-        document.documentElement.style.overflowY = "auto";
-      }
-
-      getInTouch.classList.remove("opened");
-      getInTouch.classList.add("closed");
-
-      for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("bg-bottom");
-      }
-
-      setTimeout(function () {
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].classList.remove("get_in_touch__opened");
-        }
-      }, 1500);
-    } else {
-      //open
-      if (window.innerWidth <= 848) {
-        heroTextMobile.classList.add("scrolled");
-        heroArrowMobile.classList.add("scrolled");
-        leftSide.style.transform = "translateX(100%)";
-        document.documentElement.style.overflowY = "hidden";
-      }
-      btn.textContent = "Close";
-      getInTouch.classList.remove("closed");
-      getInTouch.classList.add("opened");
-
-      for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.add("get_in_touch__opened");
-      }
-
-      for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.add("bg-bottom");
-      }
+  if (getInTouch.classList.contains("opened")) {
+    //close
+    btn.textContent = "Get in touch";
+    if (window.innerWidth <= 848) {
+      heroTextMobile.classList.remove("scrolled");
+      heroArrowMobile.classList.remove("scrolled");
+      leftSide.style.transform = "translateX(0%)";
+      document.documentElement.style.overflowY = "auto";
     }
-  }, 500);
+
+    getInTouch.classList.remove("opened");
+    getInTouch.classList.add("closed");
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].classList.remove("bg-bottom");
+    }
+
+    setTimeout(function () {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("get_in_touch__opened");
+      }
+    }, 1500);
+  } else {
+    //open
+    if (window.innerWidth <= 848) {
+      heroTextMobile.classList.add("scrolled");
+      heroArrowMobile.classList.add("scrolled");
+      leftSide.style.transform = "translateX(100%)";
+      document.documentElement.style.overflowY = "hidden";
+    }
+    btn.textContent = "Close";
+    getInTouch.classList.remove("closed");
+    getInTouch.classList.add("opened");
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].classList.add("get_in_touch__opened");
+    }
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].classList.add("bg-bottom");
+    }
+  }
 }
 
 function sendMassege() {
